63338607ae87906e9a9a1e81af95a9ec07b87a5b
csorez <csore.z@kore6.com>
2016-09-29 10:23:34 +0200

    added GetExternalIP method

--- IPWatcher/StartupTask.cs
+++ IPWatcher/StartupTask.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 
 namespace IPWatcher
@@ -14,6 +16,32 @@ namespace IPWatcher
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             this.deferral = taskInstance.GetDeferral();
+        }
+
+        private async Task Initialize()
+        {
+            await Config.CreateInstance();
+        }
+
+        private async Task<string> GetExternalIP()
+        {
+            HttpClient client = new HttpClient();
+
+            string body;
+
+            try
+            {
+                var response = await client.GetAsync(new Uri("http://icanhazip.com"));
+                response.EnsureSuccessStatusCode();
+                body = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                body = null;
+            }
+
+            return body;
         }
     }
 }
