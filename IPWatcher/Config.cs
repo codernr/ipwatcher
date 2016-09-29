007e24c81a4f3d26c74112484d313b83f78ce6d1
csorez <csore.z@kore6.com>
2016-09-29 10:19:02 +0200

    added properties to config + summaries

--- IPWatcher/Config.cs
+++ IPWatcher/Config.cs
@@ -9,21 +9,56 @@ namespace IPWatcher
     internal class Config
     {
         #region Config items
+
+        /// <summary>
+        /// The name of the device the app runs on (preconfigured)
+        /// </summary>
         public string DeviceName { get; set; }
 
+        /// <summary>
+        /// Period of checking external IP (preconfigured)
+        /// </summary>
         public int UpdateHours { get; set; }
 
-        public string IpAddress { get; set; }
+        /// <summary>
+        /// The site to check external IP on (preconfigured)
+        /// </summary>
+        public string ExternalIPCheckAddress { get; set; }
 
+        /// <summary>
+        /// Recipient e-mail where the updates are sent (preconfigured)
+        /// </summary>
+        public string Recipient { get; set; }
+
+        /// <summary>
+        /// SMTP server address (preconfigured)
+        /// </summary>
         public string SmtpServer { get; set; }
 
+        /// <summary>
+        /// Port number (preconfigured)
+        /// </summary>
         public int Port { get; set; }
 
+        /// <summary>
+        /// Use SSL (preconfigured)
+        /// </summary>
         public bool Ssl { get; set; }
 
+        /// <summary>
+        /// SMTP username (preconfigured)
+        /// </summary>
         public string Username { get; set; }
 
+        /// <summary>
+        /// SMTP password (preconfigured)
+        /// </summary>
         public string Password { get; set; }
+
+        /// <summary>
+        /// External ip of the device
+        /// </summary>
+        public string IpAddress { get; set; }
         #endregion
 
         public static Config Instance { get; private set; }
